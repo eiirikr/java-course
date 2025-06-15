fb4385b3e5d71f9704e6f14913802ad22ceb6d74
javierjericm <javierjericm@gmail.com>
2025-06-15 10:05:55 +0800

    println

---
+++ src/Println.java
@@ -0,0 +1,18 @@
+public class Println {
+    public static void main(String[] args) {
+
+        // THIS MY FIRST JAVA PROGRAM
+
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+        */
+
+        System.out.println("I like pizza!");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
+    }
+}
