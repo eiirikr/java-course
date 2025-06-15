c242695c62ff26ac5ca17c8db82cd846b8f6abfe
javierjericm <javierjericm@gmail.com>
2025-06-15 10:51:06 +0800

    variables

---
+++ src/Variables.java
@@ -0,0 +1,76 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 22;
+        int year = 2025;
+        int quantity = 1;
+
+        //System.out.println(age);
+        //System.out.println(year);
+        //System.out.println(quantity);
+
+        //System.out.println("The year is " + year);
+
+        double price = 19999.99;
+        double gwa = 98.4;
+        double temperature = -12.5;
+
+        //System.out.println("$" + price);
+        //System.out.println(gwa);
+        //System.out.println(temperature);
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        //System.out.println(grade);
+        //System.out.println(symbol);
+        //System.out.println(currency);
+
+        boolean isStudent = false;
+        boolean forSale = true;
+        boolean isOnline = true;
+
+        /*if(isStudent) {
+            System.out.println("You are a student!");
+        } else if (!isStudent){
+            System.out.println("You are not a student!");
+        }*/
+
+        String name = "Jeric";
+        String food = "pizza";
+        String email = "fake123@gmail.com";
+        String car = "Mustang";
+        String color = "black";
+
+//        System.out.println("My name is " + name);
+//        System.out.println("Your favorite food is " + food);
+//        System.out.println("Your email is " + email);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("Your gwa is: " + gwa);
+//        System.out.println("Your average letter grade is: " + grade);
+//
+//        System.out.println("Your choice is a " + color + " " + year + " " + car);
+//        System.out.println("The prize is: " + currency + price);
+//
+//        if(forSale) {
+//            System.out.println("There is a " + car + " for sale");
+//        } else {
+//            System.out.println("The " + car + " is not for sale");
+//        }
+
+        String fullName = "Jeric Javier";
+        int gta = 6;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+        if (isAdmin) {
+            System.out.println("Welcome back, " + fullName + "!");
+        } else {
+            System.out.println("Access denied!");
+        }
+
+
+    }
+}
