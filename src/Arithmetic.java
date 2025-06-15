4d5ced41f7b3c2f5fc02c304badb7ce7406aec44
javierjericm <javierjericm@gmail.com>
2025-06-15 21:17:24 +0800

    Activity: MadLibs

---
+++ src/Arithmetic.java
@@ -0,0 +1,32 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+
+//        System.out.println("Addition: " + (x + y));
+//        System.out.println("Subtraction: " + (x - y));
+//        System.out.println("Multiplication: " + (x * y));
+//        System.out.println("Division: " + (x / y));
+//        System.out.println("Remainder: " + (x % y));
+
+        // Augmented Assignment Operators
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // Increment and Decrement Operators
+        // x++;
+        // x--;
+
+        // Order of Operations [P-E-M-D-A-S]
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+
+    }
+}
