aba0579a8375dce4a5b2b604bc61889647eeeb99
javierjericm <javierjericm@gmail.com>
2025-06-16 10:09:36 +0800

    Math

---
+++ src/MathClass.java
@@ -0,0 +1,21 @@
+public class MathClass {
+    public static void main(String[] args) {
+
+//        System.out.println(Math.PI); // pi
+//        System.out.println(Math.E); // exponential
+
+        double result;
+
+        result = Math.pow(2, 3); // 2 to the power of 3
+        result = Math.abs(-5); // converts negative to positive
+        result = Math.sqrt(9); // for square root
+        result = Math.round(3.14); // round off
+        result = Math.ceil(3.14); // round up
+        result = Math.floor(3.99); // round down
+        result = Math.max(10, 20); // greater number
+        result = Math.min(10, 20); // lowest number
+
+        System.out.println(result);
+
+    }
+}
