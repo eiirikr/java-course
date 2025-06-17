bfb12cb7031f2b5810770dbf56fd3162d268179f
javierjericm <javierjericm@gmail.com>
2025-06-17 13:45:30 +0800

    Ternary Operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,17 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+//        int score = 70;
+//        String result = (score >= 60) ? "PASS": "FAIL";
+//        System.out.println(result);
+
+//        int number = 10;
+//        String evenOrOdd = (number % 2 == 0) ? "EVEN": "ODD";
+//        System.out.println(evenOrOdd);
+
+        int hours = 12;
+        String timeOfDay = (hours < 12) ? "AM": "PM";
+        System.out.println(timeOfDay);
+
+    }
+}
