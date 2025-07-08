e86892674d0205522d94306376fbcab1db35fd09
javierjericm <javierjericm@gmail.com>
2025-07-08 23:10:44 +0800

    static Keyword

---
+++ src/StaticKeyword/Main.java
@@ -0,0 +1,18 @@
+package StaticKeyword;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Friend friend1 = new Friend("Jane");
+        Friend friend2 = new Friend("John");
+
+        Friend.showFriends();
+
+        System.out.println(Friend.numOfFriends);
+
+
+
+//        Math.round(3.99);
+
+    }
+}
