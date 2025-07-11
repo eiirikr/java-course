abbaa82b2e98f299cb2c86ee33e7d1590af474d9
javierjericm <javierjericm@gmail.com>
2025-07-11 21:25:29 +0800

    Polymorphism

---
+++ src/Polymorphism/Boat.java
@@ -0,0 +1,10 @@
+package Polymorphism;
+
+public class Boat implements Vehicle{
+
+    @Override
+    public void go(){
+        System.out.println("You sail the boat");
+    }
+
+}
---
+++ src/Polymorphism/Vehicle.java
@@ -0,0 +1,7 @@
+package Polymorphism;
+
+public interface Vehicle {
+
+    void go();
+
+}
