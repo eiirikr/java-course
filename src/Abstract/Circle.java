f76936d02b6d99dd0898c2ffb0a83ce3c6a33ec6
javierjericm <javierjericm@gmail.com>
2025-07-11 20:53:05 +0800

    abstract

---
+++ src/Abstract/Circle.java
@@ -0,0 +1,16 @@
+package Abstract;
+
+public class Circle extends Shape{
+
+    double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    double area(){
+        return Math.PI * radius * radius;
+    }
+
+}
---
+++ src/Abstract/Shape.java
@@ -0,0 +1,11 @@
+package Abstract;
+
+public abstract class Shape {
+
+    abstract double area(); // ABSTRACT
+
+    void display(){ // CONCRETE
+        System.out.println("This is a shape");
+    }
+
+}
