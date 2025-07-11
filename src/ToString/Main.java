7519ad64f5b2015cd822dbc4376c9a7b8cb8c809
javierjericm <javierjericm@gmail.com>
2025-07-11 20:36:17 +0800

    .toString() Method

---
+++ src/ToString/Main.java
@@ -0,0 +1,13 @@
+package ToString;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Ford", "Mustang", 2025, "Blue");
+        Car car2 = new Car("Chevrolet", "Corvette", 2024, "Red");
+
+        System.out.println(car1);
+        System.out.println(car2);
+
+    }
+}
