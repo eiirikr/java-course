88b1712b6160117321f4271a4161933fe39c91b9
javierjericm <javierjericm@gmail.com>
2025-07-11 21:10:24 +0800

    interface

---
+++ src/Interface/Main.java
@@ -0,0 +1,17 @@
+package Interface;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+        hawk.hunt();
+
+        fish.flee();
+        fish.hunt();
+
+    }
+}
