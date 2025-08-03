d098608a25fc0f13532bbd2405598324bea6bbaf
javierjericm <javierjericm@gmail.com>
2025-08-03 21:59:57 +0800

    BufferedReader And FileReader

---
+++ src/BufferedReaderAndFileReader.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderAndFileReader {
+    public static void main(String[] args) {
+
+        String filePath = "C:\\Users\\Jessa Mae\\OneDrive\\Desktop\\test.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong");
+        }
+    }
+}
