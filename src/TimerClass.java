eeb054ad8317b589baa43dbca7d7c4c03a71f494
javierjericm <javierjericm@gmail.com>
2025-08-06 10:49:42 +0800

    Timer and TimerTask Class

---
+++ src/TimerClass.java
@@ -0,0 +1,26 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerClass {
+    public static void main(String[] args) {
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+
+            int count = 3;
+
+            @Override
+            public void run() {
+                System.out.println(count);
+                count--;
+                if(count <= 0){
+                    System.out.println("Task Complete!");
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.schedule(task, 0, 1000);
+
+    }
+}
