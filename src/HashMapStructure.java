0987c45a0a540d3e797f2127eaabdbc67d78c8f2
javierjericm <javierjericm@gmail.com>
2025-08-09 22:17:33 +0800

    HashMap Class

---
+++ src/HashMapStructure.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class HashMapStructure {
+    public static void main(String[] args) {
+
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put("apple", 0.50);
+        map.put("orange", 0.75);
+        map.put("banana", 0.25);
+        map.put("coconut", 1.00);
+
+//        map.remove("apple");
+//        System.out.println(map.get("apple"));
+//        System.out.println(map.containsKey("apple"));
+
+//        if(map.containsKey("pineapple")){
+//            System.out.println(map.get("apple"));
+//        }
+//        else{
+//            System.out.println("Key not found!");
+//        }
+
+//        System.out.println(map.containsValue(1.00));
+//        System.out.println(map.size());
+
+        for(String key : map.keySet()){
+            System.out.printf("%s : $%.2f\n",key, map.get(key));
+        }
+    }
+}
